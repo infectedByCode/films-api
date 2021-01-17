2a7d0500fa015c9b4facc52c8826c097b5ea5687
Matt Daley <mdaleyhk@gmail.com>
2021-01-17 20:09:58 +0000

    sets up Typescript for the project

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-app.listen(process.env.PORT, () => {
-  process.stdout.write(`Server running on ${process.env.PORT}`);
-});
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  if (req.url === '/') return res.send({ msg: 'hello' });
+  else next();
+});
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  res.status(404);
+  return res.send({ msg: 'not found' });
+});
+
+app.listen(process.env.PORT, () => {
+  process.stdout.write(`Server running on ${process.env.PORT}`);
+});
