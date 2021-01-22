8cf73e549292f9eeb4ee52a71a7c14f1986aa5cf
Matt Daley <mdaleyhk@gmail.com>
2021-01-22 20:05:40 +0000

    defines db types

---
+++ src/db/types.d.ts
@@ -0,0 +1,29 @@
+import knex from 'knex';
+
+interface Film {
+  uid: string;
+  title: string;
+  description: string;
+  year: number;
+  keywords: string[];
+}
+
+interface User {
+  uid: string;
+  email: string;
+  username: string;
+}
+
+interface UserCredentials {
+  readonly id: number;
+  username: string;
+  password: string;
+}
+
+declare module 'knex/types/tables' {
+  interface Tables {
+    films: Film;
+    users: User;
+    user_cred: UserCredentials;
+  }
+}
