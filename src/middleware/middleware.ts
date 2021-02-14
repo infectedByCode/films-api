9a205f68f51992457a7bf762e921cd7f8dc80c4e
Matt Daley <mdaleyhk@gmail.com>
2021-02-14 22:34:12 +0000

    adds auth middleware for routes with tests

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,5 +1,19 @@
-import { RequestHandler } from 'express';
+import { RequestHandler, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const methodNotAllowedResponder: RequestHandler<{}, { msg: string }> = (req, res, next) => {
   return res.status(405).send({ msg: 'method not allowed' });
+};
+
+export const checkAuth: RequestHandler = async (req, res, next): Promise<Response | void> => {
+  try {
+    if (req.headers.authorization) {
+      const token = req.headers.authorization;
+      jwt.verify(token, 'secret');
+      return next();
+    }
+    return res.status(403).send({ auth: false, msg: 'no token provided' });
+  } catch (err) {
+    return res.status(401).send({ auth: false, msg: 'failed to authenticate' });
+  }
 };
--- src/spec/middleware/middleware.spec.ts
+++ src/spec/middleware/middleware.spec.ts
@@ -1,7 +1,16 @@
+import sinon from 'sinon';
 import httpMocks from 'node-mocks-http';
+
+import jwt from 'jsonwebtoken';
 import * as middleware from '../../middleware/middleware';
 
 describe('#middleware', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  afterAll(() => {
+    sinon.restore();
+  });
   describe('#methodNotAllowedResponder', () => {
     test('it returns status 405 with msg in response body', () => {
       const req = httpMocks.createRequest({});
@@ -10,6 +19,38 @@ describe('#middleware', () => {
       const result = middleware.methodNotAllowedResponder(req, res, next);
       expect(res.statusCode).toBe(405);
       expect(res._getData()).toStrictEqual({ msg: 'method not allowed' });
+    });
+  });
+  describe('#checkAuth', () => {
+    test('it calls next if authorization is passed and verified', () => {
+      const req = httpMocks.createRequest({ headers: { authorization: 'mytoken' } });
+      const res = httpMocks.createResponse({});
+      const next = jest.fn();
+      sinon.stub(jwt, 'verify').callsFake(() => {
+        return Promise.resolve({});
+      });
+
+      middleware.checkAuth(req, res, next);
+      expect(next).toBeCalled();
+    });
+    test('it returns 401 if verify fails', () => {
+      const req = httpMocks.createRequest({ headers: { authorization: 'mytoken' } });
+      const res = httpMocks.createResponse({});
+      const next = jest.fn();
+      sinon.stub(jwt, 'verify').throws('error');
+
+      middleware.checkAuth(req, res, next);
+      expect(res.statusCode).toBe(401);
+      expect(res._getData()).toStrictEqual({ auth: false, msg: 'failed to authenticate' });
+    });
+    test('it returns 403 if not token passed', () => {
+      const req = httpMocks.createRequest({ headers: {} });
+      const res = httpMocks.createResponse({});
+      const next = jest.fn();
+
+      middleware.checkAuth(req, res, next);
+      expect(res.statusCode).toBe(403);
+      expect(res._getData()).toStrictEqual({ auth: false, msg: 'no token provided' });
     });
   });
 });
